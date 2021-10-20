660e25ca65d41a813aaf448e256dc1eb20c2a5ae
Stevenbarnett1 <Stevenbarnett1@live.com>
2021-10-20 15:25:27 -0700

    finished login page

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
 import "./Login.css"
+import { NavLink } from 'react-router-dom';
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
@@ -37,7 +38,9 @@ const LoginForm = () => {
         <img id = "login-page-image" src = "https://cdn.robinhood.com/assets/generated_assets/632fcb3e7ed928b2a960f3e003d10b44.jpg"></img>
       </div>
       <div id = "right-side">
-        <form onSubmit={onLogin}>
+        <div id = "right-side-inner">
+          <div id = "login-title">Welcome to Robbinghood</div>
+        <form id = "login-form" onSubmit={onLogin}>
         <div>
           {errors.map((error, ind) => (
             <div key={ind}>{error}</div>
@@ -46,6 +49,7 @@ const LoginForm = () => {
         <div>
           <label htmlFor='email'>Email</label>
           <input
+            autoComplete="off"
             name='email'
             type='text'
             placeholder='Email'
@@ -55,16 +59,24 @@ const LoginForm = () => {
         </div>
         <div>
           <label htmlFor='password'>Password</label>
-          <input
+          <input id = "test"
+          autoComplete="off"
             name='password'
             type='password'
             placeholder='Password'
             value={password}
             onChange={updatePassword}
           />
-          <button type='submit'>Login</button>
+        </div>
+        <div id = "button-container">
+          <button type='submit'>Sign In</button>
+          <div>
+            Not on Robbinghood?
+            <NavLink to = "/sign-up"> Create an account</NavLink>
+          </div>
         </div>
       </form>
+      </div>
       </div>
 
     </div>
