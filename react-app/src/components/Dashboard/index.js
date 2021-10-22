3c7277773280ba4f53f057567d24fab576b2a41e
Stevenbarnett1 <Stevenbarnett1@live.com>
2021-10-22 10:35:41 -0700

    added basic graph, adding data based on time intervals next

--- react-app/src/components/Dashboard/index.js
+++ react-app/src/components/Dashboard/index.js
@@ -1,18 +1,23 @@
 import {useSelector,useDispatch} from "react-redux"
 import { useState, useEffect } from "react"
 import "./Dashboard.css"
+import {
+    LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, PieChart, Pie
+  } from 'recharts';
 const finnhub = require('finnhub');
 const api_key = finnhub.ApiClient.instance.authentications['api_key'];
 api_key.apiKey = "c5mtisqad3iam7tur1qg"
 const finnhubClient = new finnhub.DefaultApi()
+
 
 const Dashboard = () => {
     const dispatch = useDispatch()
     const [portfolioValue,setPortfolioValue] = useState(0.00)
     const [buyingPower,toggleBuyingPower] = useState(false)
     const [openLists,setOpenLists] = useState([])
+    const [currentPrices,setCurrentPrices] = useState({})
+    const [graphData,setGraphData] = useState("")
     const user = useSelector(state=>state.session.user)
-
     useEffect(()=>{
         if(user){
             let total = 0
@@ -30,36 +35,83 @@ const Dashboard = () => {
         console.log(portfolioValue)
     },[portfolioValue])
 
+
     const toggleOpenLists = (watchlist) => {
         let newList = []
+        let prices = {}
         let found = false
+
+        for(let watchlist_stock of watchlist.stocks){
+            finnhubClient.quote(watchlist_stock.symbol, (error, data, response) => {
+                prices[watchlist_stock.symbol]=data.c
+                console.log("PRICES: ",prices)
+                setCurrentPrices(prices)
+            })
+        }
+
+
         for(let i=0;i<openLists.length;i++){
             if(openLists[i] !== watchlist.id){
-                newList.append(watchlist.id)
+                newList.push(openLists[i])
             } else {
                 found = true
             }
         }
-        if (!found)newList.append(watchlist.id)
+
+        if (!found){
+            newList.push(watchlist.id)
+
+        }
         setOpenLists(newList)
 
-    }
 
-    console.log(user)
-    console.log(buyingPower)
+
+    }
+    const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400},{name: 'Page B', uv: 500, pv: 2000, amt: 2000},{name: 'Page C', uv: 600, pv: 1500, amt: 1500}];
+
+    useEffect(()=>{
+        let start = new Date()
+        start.setHours(0,0,0,0)
+        let end = new Date()
+        end.setHours(23,59,59,999)
+        let startUnix = start.getTime() / 1000
+        let endUnix = start.getTime() / 1000
+        finnhubClient.stockCandles("AAPL", "15", startUnix, endUnix, (error, data, response) => {
+
+          });
+    },[])
+
+
+    const renderLineChart = (
+        <LineChart width={400} height={400} data={data}>
+          <Line type="monotone" dataKey="amt" stroke="#8884d8" />
+          <XAxis dataKey="name" angle={0} textAnchor="end" tick={{ fontSize: 13 }} />
+        </LineChart>
+      );
+
+    console.log("USER: ",user)
+    console.log("openlists: ",openLists)
+    console.log("CURRENT PRICES: ",currentPrices)
     return (
         <div id = "dashboard-outer-container">
             <div id = "dashboard-left-container">
                 <div id = "dashboard-upper-container">
-                    <div id = "dashboard-portfolio-value"><h1>${portfolioValue}</h1></div>
+                    <div id = "dashboard-portfolio-value"><h1>${portfolioValue.toFixed(2)}</h1></div>
                     <div id = "dashboard-graph-container">
-                        <div id = "dashboard-graph"></div>
-                        <div id = "dashboard-graph-timeframes-container"></div>
+                        <div id = "dashboard-graph">{renderLineChart}</div>
+                        <div id = "dashboard-graph-timeframes-container">
+                            <span className = "dashboard-graph-timeframe">1D</span>
+                            <span className = "dashboard-graph-timeframe">1W</span>
+                            <span className = "dashboard-graph-timeframe">1M</span>
+                            <span className = "dashboard-graph-timeframe">3M</span>
+                            <span className = "dashboard-graph-timeframe">1Y</span>
+                            <span className = "dashboard-graph-timeframe">ALL</span>
+                        </div>
                     </div>
                     <div id = "dashboard-buying-power-container" onClick={()=>toggleBuyingPower(!buyingPower)} >
                         <div id = "dashboard-buying-power-container-heading">
                             <div id = "dashboard-buying-power-text">Buying Power</div>
-                            <div id = "dashboard-buying-power-value" style = {buyingPower ? {display:"none"} : {display:"block"}}>${user && user.buyingPower}</div>
+                            <div id = "dashboard-buying-power-value" style = {buyingPower ? {display:"none"} : {display:"block"}}>${(user && user.buyingPower) ? user.buyingPower.toFixed(2) : 0.00.toFixed(2)}</div>
                         </div>
                         <div id = "dashboard-buying-power-container-bottom" style = {buyingPower ? {display:"flex"} : {display:"none"}}>
                             <div id = "dashboard-buying-power-container-left">
@@ -69,7 +121,7 @@ const Dashboard = () => {
                                 </div>
                                 <div id = "buying-power-container">
                                     <div>Buying Power</div>
-                                    <div>{user && user.buyingPower}</div>
+                                    <div>${(user && user.buyingPower) ? user.buyingPower.toFixed(2) : 0.00.toFixed(2)}</div>
                                 </div>
                                 <button id = "buying-power-deposit-button">Deposit Funds</button>
                             </div>
@@ -110,18 +162,17 @@ const Dashboard = () => {
                     {user && user.watchlists.map(watchlist=>{
                         return (
                             <div key = {watchlist.id} className = "watchlist-inner-container">
-                                <div className = "watchlist-title" onClick = {(watchlist)=>toggleOpenLists(watchlist)}>{watchlist.name}</div>
+                                <div className = "watchlist-title" onClick = {()=>toggleOpenLists(watchlist)}>{watchlist.name}</div>
                                 <div className = "watchlist-stocks">
                                     {watchlist.stocks.map(stock=>{
                                         return (
-                                            <div key = {stock.id} className = "watchlist-stock-container" style = {openLists.includes(watchlist.id) ? {display:"block"} : {display:"none"}}>
-                                                <div className = "watchlist-stock-symbol"></div>
-                                                    <div className = "watchlist-graph"></div>
-                                                    <div className = "watchlist-stock-price-container">
-                                                        <div className = "watchlist-stock-price"></div>
-                                                        <div className = "watchlist-stock-change"></div>
-                                                    </div>
-
+                                            <div key = {stock.id} className = "watchlist-stock-container" style = {openLists.includes(watchlist.id) ? {display:"flex"} : {display:"none"}}>
+                                                <div className = "watchlist-stock-symbol">{stock.symbol}</div>
+                                                <div className = "watchlist-graph">{}</div>
+                                                <div className = "watchlist-stock-price-container">
+                                                    <div className = "watchlist-stock-price">{currentPrices[stock.symbol]}</div>
+                                                    <div className = "watchlist-stock-change"></div>
+                                                </div>
                                             </div>
                                         )
                                     })}
